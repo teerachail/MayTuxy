030f0fead7912590656e47d24558b5a2a4754ce9
natit <natit55@hotmail.com>
2011-01-14 14:53:39 +0700

    แก้ Clock step 2 และ หน้ารวม
    Signed-off-by: natit <natit55@hotmail.com>

--- src/MayaTukky/TheS.SperfGames.MayaTukky/Controls/ClockUI.xaml.cs
+++ src/MayaTukky/TheS.SperfGames.MayaTukky/Controls/ClockUI.xaml.cs
@@ -17,6 +17,67 @@ namespace TheS.SperfGames.MayaTukky.Controls
         public ClockUI()
         {
             InitializeComponent();
+            clockFive.Sb_SandFall.Completed += new EventHandler(Sb_SandFall_Completed);
+            clockOne.Sb_Light.Completed += new EventHandler(Sb_Light_Completed);
         }
+
+        private void Sb_Light_Completed(object sender, EventArgs e)
+        {
+            stopPlay();
+            var temp = LastCombo;
+            if (temp != null)
+            {
+                temp(this, EventArgs.Empty);
+            }
+        }
+
+        private void Sb_SandFall_Completed(object sender, EventArgs e)
+        {
+            PlayLight();
+        }
+        public void PlayClockOne()
+        {
+            clockOne.Sb_SandFall.Begin();
+        }
+        public void PlayClockTwo()
+        {
+            clockTwo.Sb_SandFall.Begin();
+        }
+        public void PlayClockThree()
+        {
+            clockThree.Sb_SandFall.Begin();
+        }
+        public void PlayClockFour()
+        {
+            clockFour.Sb_SandFall.Begin();
+        }
+        public void PlayClockFive()
+        {
+            clockFive.Sb_SandFall.Begin();
+        }
+        public void PlayLight()
+        {
+            clockOne.Sb_Light.Begin();
+            clockTwo.Sb_Light.Begin();
+            clockThree.Sb_Light.Begin();
+            clockFour.Sb_Light.Begin();
+            clockFive.Sb_Light.Begin();
+        }
+        private void stopPlay()
+        {
+            clockOne.Sb_SandFall.Stop();
+            clockTwo.Sb_SandFall.Stop();
+            clockThree.Sb_SandFall.Stop();
+            clockFour.Sb_SandFall.Stop();
+            clockFive.Sb_SandFall.Stop();
+
+            clockOne.Sb_Light.Stop();
+            clockTwo.Sb_Light.Stop();
+            clockThree.Sb_Light.Stop();
+            clockFour.Sb_Light.Stop();
+            clockFive.Sb_Light.Stop();
+        }
+
+        public event EventHandler LastCombo;
     }
 }
